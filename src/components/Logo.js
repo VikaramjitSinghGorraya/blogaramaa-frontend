eb561a5712386c72f2119217b898d6e5e3db75fb
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-01-01 21:12:22 -0700

    created Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import { Link } from '@chakra-ui/react';
+const Logo = () => {
+	return (
+		<div className='logo'>
+			<Link href='/'>
+				<span>BLOGA</span>RAMAA
+			</Link>
+		</div>
+	);
+};
+
+export default Logo;
