96015361c184036cab0a79ce6a201bb3fb05b0be
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-01-02 22:31:51 -0700

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { VStack } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 function App() {
 	return (
 		<>
 			<Navbar />
+			<VStack h='100vh' w='90%' m='auto'>
+				<Routes>
+					<Route path='/Signup' element={<Signup />} />
+					<Route path='/Signin' element={<Signin />} />
+				</Routes>
+			</VStack>
 		</>
 	);
 }
