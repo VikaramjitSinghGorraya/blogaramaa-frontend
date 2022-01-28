a308672d938c7fca1b6ea06e8818ef81ccd1a5df
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-01-28 09:50:41 -0700

    Created share component to share post

---
+++ src/components/Share.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Image, Text } from '@chakra-ui/react';
+import { RWebShare } from 'react-web-share';
+import { ShareInof } from '../types/Share';
+import share from '../icons/share.svg';
+const Share = ({ showText = false }: ShareInof) => {
+	return (
+		<RWebShare
+			data={{
+				text: 'text',
+				url: 'url',
+				title: 'title',
+			}}
+		>
+			<Button variant='base' w='7rem'>
+				<Image className='mutedIconColor' src={share} />
+				{showText ? (
+					<Text as='p' color='brand.mutedText'>
+						Share
+					</Text>
+				) : (
+					''
+				)}
+			</Button>
+		</RWebShare>
+	);
+};
+
+export default Share;
