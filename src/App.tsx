eeff0bee0600f69140a4a3fdf6eb121f907df13c
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-01-28 09:53:03 -0700

    Added links to all pages in App. Added libraries

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/RestePassword';
 import Blog from './pages/Blog';
 import Search from './pages/Search';
+import Home from './pages/Home';
+import EditProfile from './pages/EditProfile';
+import Profile from './pages/Profile';
+import Post from './pages/Post';
+import Contact from './pages/Contact';
 
 function App() {
 	return (
@@ -14,12 +19,17 @@ function App() {
 			<Navbar />
 			<VStack h='100vh' w='90%' m='auto'>
 				<Routes>
+					<Route path='/' element={<Home />} />
 					<Route path='/Signup' element={<Signup />} />
 					<Route path='/Signin' element={<Signin />} />
 					<Route path='/ForgotPassword' element={<ForgotPassword />} />
 					<Route path='/ResetPassword' element={<ResetPassword />} />
 					<Route path='/WriteBlog' element={<Blog />} />
 					<Route path='/Search' element={<Search />} />
+					<Route path='/Edit' element={<EditProfile />} />
+					<Route path='/Profile' element={<Profile />} />
+					<Route path='/Post' element={<Post />} />
+					<Route path='/Contact' element={<Contact />} />
 				</Routes>
 			</VStack>
 		</>
