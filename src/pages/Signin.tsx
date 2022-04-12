f48ce22eff6e2c539de874753402da8903276694
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-04-11 19:28:11 -0600

    Added display error ; check if user logged in

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -8,8 +8,9 @@ import {
 	Image,
 	Heading,
 } from '@chakra-ui/react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useSignin } from '../queries/Queries';
+import { Navigate } from 'react-router-dom';
+import MessageBox from '../components/MessageBox';
+import { useSignin, useIsLoggedIn } from '../queries/Queries';
 import go from '../icons/go.svg';
 import InputField from '../components/InputField';
 
@@ -19,9 +20,8 @@ const Signin = () => {
 		password: '',
 		inputCleared: false,
 	});
-
 	const signinProcess = useSignin();
-	const navigate = useNavigate();
+	const { status: loggedInStatus } = useIsLoggedIn();
 
 	const inputChangeHandler = (e) => {
 		setUserData({ ...userData, [e.target.name]: e.target.value });
@@ -33,49 +33,70 @@ const Signin = () => {
 		signinProcess.mutate(userData);
 	};
 
-	const redirectToHome = () => {
-		navigate('/');
+	const displayErrorMessage = () => {
+		return (
+			<MessageBox
+				toastId='error-id'
+				title='The following error occured.'
+				description={signinProcess.error}
+				successStatus={false}
+			/>
+		);
 	};
-	return (
+
+	return loggedInStatus === 'success' ? (
+		<Navigate to='/' />
+	) : (
 		<VStack w={['100%', '50%']} spacing={25} m='auto'>
 			<Heading as='h2' color='brand.primaryBlue'>
 				Welcome Back!
 			</Heading>
-			<InputField
-				name='email'
-				type='email'
-				placeholder='Email'
-				value={userData.email}
-				onChange={inputChangeHandler}
-			/>
-			<InputField
-				name='password'
-				type='password'
-				placeholder='Password'
-				value={userData.password}
-				onChange={inputChangeHandler}
-			/>
-			<Link href='/forgotPassword' variant='blueLink' alignSelf='flex-start'>
-				FORGOT PASSWORD
-			</Link>
-			<HStack w='100%' justifyContent='space-between'>
-				<VStack alignItems='flex-start'>
-					<Text as='small' color='brand.mutedText'>
-						Yet to join?
-					</Text>
-					<Link href='/signup' variant='blueLink'>
-						SIGN UP
+			<form style={{ width: '100%' }} onSubmit={submitHandler}>
+				<VStack w='100%' h='100%' justifyContent='space-around'>
+					<InputField
+						name='email'
+						type='email'
+						placeholder='Email'
+						value={userData.email}
+						onChange={inputChangeHandler}
+					/>
+					<InputField
+						name='password'
+						type='password'
+						placeholder='Password'
+						value={userData.password}
+						onChange={inputChangeHandler}
+					/>
+					<Link
+						href='/forgotPassword'
+						variant='blueLink'
+						alignSelf='flex-start'
+					>
+						FORGOT PASSWORD
 					</Link>
 				</VStack>
-				<Button
-					variant='round'
-					onClick={submitHandler}
-					isLoading={signinProcess.isLoading ? true : false}
-				>
-					<Image src={go} />
-				</Button>
-			</HStack>
-			{/* {signinProcess.isSuccess && redirectToHome()} */}
+				<HStack w='100%' justifyContent='space-between'>
+					<VStack alignItems='flex-start'>
+						<Text as='small' color='brand.mutedText'>
+							Yet to join?
+						</Text>
+						<Link href='/signup' variant='blueLink'>
+							SIGN UP
+						</Link>
+					</VStack>
+					<Button
+						variant='round'
+						type='submit'
+						isLoading={signinProcess.isLoading ? true : false}
+					>
+						<Image src={go} />
+					</Button>
+				</HStack>
+			</form>
+			{signinProcess.isSuccess && <Navigate to='/' />}
+			{signinProcess.isError &&
+				!signinProcess.isLoading &&
+				displayErrorMessage()}
 		</VStack>
 	);
 };
