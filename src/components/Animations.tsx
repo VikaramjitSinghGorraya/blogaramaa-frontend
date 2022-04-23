5279c1f8be5b769ba6275da4e8e9daf67c883725
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-04-23 14:18:02 -0600

    Defined animation styles.

---
+++ src/components/Animations.tsx
@@ -0,0 +1,6 @@
+export const pageDisplayAnimation = {
+	pos: 'relative',
+	initial: { top: 18, opacity: 0 },
+	animate: { top: 0, opacity: 1 },
+	transition: { delay: 0, duration: 0.3, ease: 'easeIn' },
+};
