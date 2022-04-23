a0ee6bc51a15bc23e09a2c680423425a10823168
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-04-22 19:36:19 -0600

    Fixed layout for heading.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,10 +4,12 @@ import { BannerInfo } from '../types/Banner';
 
 const Banner = ({ heading, icon }: BannerInfo) => {
 	return (
-		<HStack w='100%' justifyContent='space-between' px={['0', '5']}>
-			<Heading as='h1' color='brand.mutedTextLight'>
-				{heading}
-			</Heading>
+		<HStack w='100%' justifyContent='space-between' px={['0', '5']} wrap='wrap'>
+			<HStack w='70%'>
+				<Heading as='h1' color='brand.mutedTextLight' wordBreak='break-all'>
+					{heading}
+				</Heading>
+			</HStack>
 			<Image src={icon} h={['5rem', '12rem']} w={['5rem', '12rem']} />
 		</HStack>
 	);
