e5a2c138b19165c31787264a46142ca8ca4796be
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-04-25 14:13:15 -0600

    Changed header layout for better fit.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,9 +4,9 @@ import { BannerInfo } from '../types/Banner';
 
 const Banner = ({ heading, icon, isProfilePic }: BannerInfo) => {
 	return (
-		<HStack w='100%' justifyContent='space-between' px={['0', '5']} wrap='wrap'>
+		<HStack w='100%' justifyContent='space-between' px={['0', '5']}>
 			<HStack w='70%'>
-				<Heading as='h1' color='brand.mutedTextLight' wordBreak='break-all'>
+				<Heading as='h1' color='brand.mutedTextLight' wordBreak='break-word'>
 					{heading}
 				</Heading>
 			</HStack>
