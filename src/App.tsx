7a71fb450bccf3f2fa5d3c0c6780ca2e409b6c0c
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-04-26 12:36:43 -0600

    Lazy loaded the pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { VStack } from '@chakra-ui/react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { displayTabTitle } from './helpers/Functions';
 import Navbar from './components/Navbar';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/RestePassword';
-import Blog from './pages/Blog';
-import Search from './pages/Search';
 import Home from './pages/Home';
-import EditProfile from './pages/EditProfile';
-import Profile from './pages/Profile';
-import Post from './pages/Post';
-import Contact from './pages/Contact';
-import AccountActivation from './pages/AccountActivation';
-import EditBlog from './pages/EditBlog';
-import OtherUserProfile from './pages/OtherUserProfile';
-
+const Signin = lazy(() => import('./pages/Signin'));
+const Signup = lazy(() => import('./pages/Signup'));
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
+const ResetPassword = lazy(() => import('./pages/RestePassword'));
+const Blog = lazy(() => import('./pages/Blog'));
+const Search = lazy(() => import('./pages/Search'));
+const EditProfile = lazy(() => import('./pages/EditProfile'));
+const Post = lazy(() => import('./pages/Post'));
+const Contact = lazy(() => import('./pages/Contact'));
+const AccountActivation = lazy(() => import('./pages/AccountActivation'));
+const EditBlog = lazy(() => import('./pages/EditBlog'));
+const OtherUserProfile = lazy(() => import('./pages/OtherUserProfile'));
+const Profile = lazy(() => import('./pages/Profile'));
 function App() {
 	const location = useLocation();
 
@@ -27,26 +26,29 @@ function App() {
 	return (
 		<>
 			<Navbar />
+
 			<VStack h='100vh' w='90%' m='auto'>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/Signup' element={<Signup />} />
-					<Route path='/Signin' element={<Signin />} />
-					<Route path='/ForgotPassword' element={<ForgotPassword />} />
-					<Route path='/ResetPassword/:token' element={<ResetPassword />} />
-					<Route path='/WriteBlog' element={<Blog />} />
-					<Route path='/EditBlog/:slug' element={<EditBlog />} />
-					<Route path='/Search' element={<Search />} />
-					<Route path='/EditProfile' element={<EditProfile />} />
-					<Route path='/Profile' element={<Profile />} />
-					<Route path='/Profile/:authorId' element={<OtherUserProfile />} />
-					<Route path='/Post/:slug' element={<Post />} />
-					<Route path='/Contact' element={<Contact />} />
-					<Route
-						path='/AccountActivation/:token'
-						element={<AccountActivation />}
-					/>
-				</Routes>
+				<Suspense fallback={null}>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/Signup' element={<Signup />} />
+						<Route path='/Signin' element={<Signin />} />
+						<Route path='/ForgotPassword' element={<ForgotPassword />} />
+						<Route path='/ResetPassword/:token' element={<ResetPassword />} />
+						<Route path='/WriteBlog' element={<Blog />} />
+						<Route path='/EditBlog/:slug' element={<EditBlog />} />
+						<Route path='/Search' element={<Search />} />
+						<Route path='/EditProfile' element={<EditProfile />} />
+						<Route path='/Profile' element={<Profile />} />
+						<Route path='/Profile/:authorId' element={<OtherUserProfile />} />
+						<Route path='/Post/:slug' element={<Post />} />
+						<Route path='/Contact' element={<Contact />} />
+						<Route
+							path='/AccountActivation/:token'
+							element={<AccountActivation />}
+						/>
+					</Routes>
+				</Suspense>
 			</VStack>
 		</>
 	);
